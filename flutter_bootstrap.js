f7d6d760f2a0f54438b06496fd6f9e572b764e63
QingheMonster <QingheMonster@users.noreply.github.com>
2026-03-26 13:30:45 +0000

    deploy: c39864c61d9158e912fd3520354a485a405e7ec3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3620264578"
+    serviceWorkerVersion: "308886319"
   }
 });
